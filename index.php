69aeca7360a691ad72c44124df0c700a81e24223
sanzar jeon <jeon4012@naver.com>
2023-03-24 16:09:01 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+    <div>
+        이제 시작이다
+    </div>
+    <span></span>slkdjkfl;sdfl;sdf<div></div>
+ </body>
+</html>
